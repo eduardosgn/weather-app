4a7a5d1b63bbdf802a4c5c654d29023d2838f4ef
Eduardo Nascimento <71605922+eduardosgn@users.noreply.github.com>
2021-07-16 18:53:08 -0300

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,29 @@
+/*
+    Código para a comunicação/interação com a API da AccuWeather
+*/
+
+const apiKey = 'hN0ilLKr21niEu1dvU1MTyVAoQaG5SvW';
+const baseUrl = 'http://dataservice.accuweather.com/';
+const getCityUrl = cityName => `${baseUrl}locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`;
+const getWeatherUrl = cityKey => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${apiKey}&language=pt-br`;
+
+const fetchData = async url => {
+    try {
+
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            throw new Error('Não foi possível obter os dados.');
+        };
+
+        return response.json();
+
+    } catch ({ name, message }) {
+
+        alert(`${name}: ${message}`);
+
+    };
+};
+
+const getCityData = cityName => fetchData(getCityUrl(cityName));
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey));
